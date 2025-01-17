955b863a0f590698658b17d1d20e7a53918a7275
Kyle Andrews <codingwithkyle@gmail.com>
2025-01-17 12:37:42 -0500

    Added checkerboard pattern & scaled shapes to grid size

--- main.c
+++ main.c
@@ -10,6 +10,7 @@ typedef float f32;
 #define FACTOR 80
 #define WIDTH (16*FACTOR)
 #define HEIGHT (9*FACTOR)
+#define CELL_SIZE 12
 
 void game_init()
 {
@@ -21,8 +22,24 @@ void game_update(f32 dt)
 {
     BeginDrawing();
     ClearBackground((Color){18,18,18,255});
-    DrawRectangle(WIDTH*0.5, HEIGHT*0.5, 100, 100, RED);
-    DrawCircle(100, 100, 500, BLUE);
+    
+    for (i32 y = 0; y < HEIGHT; y+=CELL_SIZE)
+    {
+        for(i32 x = 0; x < WIDTH; x+=CELL_SIZE)
+        {
+            if (((x/CELL_SIZE)+(y/CELL_SIZE))%2 == 0)
+            {
+                DrawRectangle(x, y, CELL_SIZE, CELL_SIZE, (Color){18,18,18,255});
+            }
+            else
+            {
+                DrawRectangle(x, y, CELL_SIZE, CELL_SIZE, (Color){32,32,32,255});
+            }
+        }
+    }
+
+    DrawRectangle(12*20, 12*20, CELL_SIZE, CELL_SIZE, RED);
+    DrawCircle(12*5-7, 12*5-7, CELL_SIZE*0.5, GREEN);
     EndDrawing();
 }
 
